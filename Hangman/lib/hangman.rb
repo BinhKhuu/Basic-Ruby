d41304e8f7395eb875a260a15bb8835f03211c0a
Binh Khuu <binhkhuu90@gmail.com>
2017-06-13 15:23:39 +0800

    finished run and display functionaltiy

--- Hangman/lib/hangman.rb
+++ Hangman/lib/hangman.rb
@@ -7,7 +7,7 @@ class Hangman
 	attr_reader :dict_path
 
 	def initialize
-		@turns = 1
+		@turns = 0
 		@win = false
 		@guess = []
 		parent = File.expand_path("..", Dir.pwd)
@@ -27,22 +27,49 @@ class Hangman
 		player = Player.new(name)
 		#generate the secret word
 		word = gen_word
-
+		bad_letters = []
+		clue = []
+		for i in 0..word.length-1
+			clue[i] = '_'
+		end	
 		#game begins
-			#ask for user to guess a letter
-
-			#check if letter is in word
-				#turn taken for correct and incorrect guess
-
-			#ask user wants to guess the word
-				#turn taken if incorrect guess
-
-			#check if game is over
+		
+		finished = false
+		while !finished do
+			puts "\nguess the word or letter\n"
+			guess = gets.chomp.downcase
+			if guess.length == 1
+				matched = false	
+				for i in 0..word.length-1			
+					if guess == word[i]
+						clue[i] = guess 
+						matched = true
+					end
+				end
+				bad_letters<< guess if !matched
+			elsif guess.length == word.length
+				#check if word and guess correct
+				if guess == word 
+					finished = true
+					puts "Game over you win!"
+				else
+					puts "Wrong!"
+				end
+			#if guess is larger than word no need to check
+			else
+				puts "CLUE! your guess is greater than #{word.length} letters try again"
+			end
+			finished = true if @turns == 11
+			@turns += 1 unless @turns == 11
+			display(clue,bad_letters)
+		end
+		puts "secret word is : #{word}"
 
 	end
 
 	#display current game
-	def display
+	def display(clue,bad_letters)
+		print "\n#{clue.join(' ')}\t bad letters: #{bad_letters.join(' ')}\t turns left: #{11 - @turns}\n"
 
 	end
 	#generate secret word
@@ -54,23 +81,17 @@ class Hangman
 			while word.length < 5 || word.length > 12
 				#return to start of file
 				f.seek(0)
-				puts "opened #{@dict_path}"
 				indx = rand(1..61406)
 				for i in 1..indx 
 					f.readline
 				end
-				word = f.gets
+				word = f.gets.chomp
 			end
 		end		
 
 		#check if word is between 5 and 12 letters
 		#return word
-		word 
-	end
-
-	private
-	def winner?
-
+		word.downcase 
 	end
 
 	#save game
