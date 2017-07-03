4325578e3273f124b7186a5b8db16f9ad4639b3c
Binh Khuu <binhkhuu90@gmail.com>
2017-07-03 11:51:05 +0800

    finished knight moves, updated readme

--- Knight_Moves/lib/knight_moves.rb
+++ Knight_Moves/lib/knight_moves.rb
@@ -1,101 +1,89 @@
+# @board: represents the chess board where each object is accessed by x,y coordinates
+#* x and y are assumed to be the same i.e the board is a square 
+# @board: hash table, key: [x,y] corrdinates, value: connected edges
 class KnightMoves
 	attr_accessor :board
 	def initialize(size)
 		@board = Hash.new
 		gen_board(size)
 	end
-
+	#generate board
+	# *size: size of board, area = size * size
 	def gen_board(size)
 		for x in 0..size-1
 			for y in 0..size-1
-				@board[[x,y]] = Square.new(x,y)
+				@board[[x,y]] = Square.new(x,y,size)
 			end
 		end
 	end
-
+	#finds one of the possible shortest path from source to destination squre
+	# *source: starting square
+	# *dest: destination square
 	def find_path(source,dest)
-		#assign source node to 0
-		source.distant = 0 
-		#set current = source
-		current = source 
-		#mark all as unvisited
+		current = @board[source] 
 		path = []
 		visited = []
 		unvisited = []
 		unvisited << current
-		path << source.name
-		#for current node look at all edges 
+		path << @board[source].name
 		while(!unvisited.empty?)
 			for i in 0..current.edges.length-1
-				#add edges to unvisited if they have not been visited
 				if !visited.include?(@board[current.edges[i]]) 
-				#keep track of path
 					@board[current.edges[i]].prev = current
 					unvisited<< @board[current.edges[i]] 
 				end
 			end
-
 			visited << current
 			unvisited.shift
-				#pick first edge
 			current = unvisited[0]
-				#if edge is the destination end
-			return current if current == dest 
+			if current == @board[dest] 
+				print_path(current)
+				return
+			end
 		end
 	end
 
-	class Square
-		attr_accessor :edges, :distant, :name, :prev
+	def print_path(node)
+		current = node
+		path = []
+		path.unshift(current.name)
+		while current.prev != nil
+			path.unshift(current.prev.name) 
+			current = current.prev
+		end
+		puts "You made it in #{path.length-1} move(s)! Here's your path:"
+		for i in 0..path.length-1
+			print "[#{path[i]}] "
+		end
+		puts ""
+	end
 
-		def initialize(x,y)
+	# @edges: array of connected nodes
+	# @name: string name of current square [x,y] cordinate
+	# @prev: name of previous connected to to build path
+	class Square
+		attr_accessor :edges, :name, :prev
+
+		def initialize(x,y,size)
 			@prev = nil
 			@name = "#{x.to_s},#{y.to_s}"
-			@edges = []
-			@distant = -1 
-			gen_edges(x,y)
+			@edges = [] 
+			gen_edges(x,y,size)
 		end
 
-		def gen_edges(x,y)
+		def gen_edges(x,y,size)
 			#add edges if knight moves are within the board
-			@edges << [x+1,y+2] if x + 1 >= 0 && x + 1 <= 3 && y + 2 >= 0 && y + 2 <= 3
-			@edges << [x+1,y-2] if x + 1 >= 0 && x + 1 <= 3 && y - 2 >= 0 
-			@edges << [x+2,y+1] if x + 2 >= 0 && x + 2 <= 3 && y + 1 >= 0 && y + 1 <= 3
- 			@edges << [x+2,y-1] if x + 2 >= 0 && x + 2 <= 3 && y - 1 >= 0 
+			@edges << [x+1,y+2] if x + 1 >= 0 && x + 1 < size && y + 2 >= 0 && y + 2 < size 
+			@edges << [x+1,y-2] if x + 1 >= 0 && x + 1 < size && y - 2 >= 0 
+			@edges << [x+2,y+1] if x + 2 >= 0 && x + 2 < size && y + 1 >= 0 && y + 1 < size 
+ 			@edges << [x+2,y-1] if x + 2 >= 0 && x + 2 < size && y - 1 >= 0 
 			@edges << [x-1,y-2] if x - 1 >= 0 && y - 2 >= 0 
-			@edges << [x-1,y+2] if x - 1 >= 0 && y + 2 >= 0 && y + 2 <= 3
+			@edges << [x-1,y+2] if x - 1 >= 0 && y + 2 >= 0 && y + 2 < size   
 			@edges << [x-2,y-1] if x - 2 >= 0 && y - 1 >= 0
-			@edges << [x-2,y+1] if x - 2 >= 0 && y + 1 >= 0 && y + 1 <= 3
+			@edges << [x-2,y+1] if x - 2 >= 0 && y + 1 >= 0 && y + 1 < size 
 		end
 	end
 end
 
 
-a = KnightMoves.new(4)
-#puts a.board
-x = a.board[[1,1]].edges[0][0]
-y = a.board[[1,1]].edges[2][2]
 
-print a.board[[3,2]].edges[0]
-puts ""
-#alorithm
-source = a.board[[0,0]]
-dest = a.board[[1,1]]
-a.find_path(source,dest)
-
-
-current = a.board[[1,1]]
-while current.prev != nil
-	
-	puts current.prev.name 
-	current = current.prev
-end
-
-
- 
-#create the 8x8 graph with edges
-
-#use dikstras alorithm to get sortest path
-
-#record paths along the way
-
-#win
---
+++ Knight_Moves/lib/test.rb
@@ -0,0 +1,5 @@
+a = KnightMoves.new(8)
+x = [3,3]
+y = [4,3]
+
+a.find_path(x,y)
